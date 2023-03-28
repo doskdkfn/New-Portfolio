171b2068c676f4dc0d5d2ec8a2051ab962cda608
doskdkfn <dawn_1216@naver.com>
2023-03-28 17:10:29 +0900

    2023-03-28 portfolio main

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/**************** 비디오 태그 autoplay 재생 **************/
+
+var autoplayVideoInterval = setInterval("autoplayVideo()", 200);
+
+
+function autoplayVideo() {
+    var promise = document.querySelector('video').play();
+    if (promise !== undefined) {
+        promise.then(function (_) {
+            // Autoplay started!
+            clearInterval(autoplayVideoInterval);
+
+        }).catch(function (error) {// Autoplay was prevented.
+            // Show a "Play" button so that user can start playback.
+        });
+    }
+}
